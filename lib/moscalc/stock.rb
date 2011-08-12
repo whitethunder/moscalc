0ebcc6779b20cd0766fb6f48348a15b9f8dca724
Matt White <mattw922@gmail.com>
2011-08-12 12:06:06 -0600

    Add growth_rate method. Update tests.

--- lib/moscalc/stock.rb
+++ lib/moscalc/stock.rb
@@ -21,6 +21,10 @@ module Moscalc
       calculate_growth_rate(eps, years)
     end
 
+    def growth_rate(years=:max)
+      [self.analyst_growth_rate, calculate_growth_rate(eps, years)].compact.min
+    end
+
     def equity_growth_rate(years=:max)
       calculate_growth_rate(equity, years)
     end
@@ -38,15 +42,15 @@ module Moscalc
     end
 
     def future_eps
-      return nil unless(eps_growth_rate && current_eps)
-      (1 + eps_growth_rate) ** Years_To_Grow * current_eps
+      return nil unless(growth_rate && current_eps)
+      (1 + growth_rate) ** Years_To_Grow * current_eps
     end
 
     # Future P/E is the minimum of the historical P/E, EPS growth rate * 2, or 50. If we can't get an EPS growth rate or
     # a historical P/E, the current P/E is a good alternative.
     def future_pe
-      if eps_growth_rate || historical_pe
-        [eps_growth_rate ? eps_growth_rate * 200.0 : nil,
+      if growth_rate || historical_pe
+        [growth_rate ? growth_rate * 200.0 : nil,
          Moscalc.ema(historical_pe + [current_pe]),
          Max_Future_PE
         ].compact.min
@@ -74,12 +78,12 @@ module Moscalc
       puts "1 - (current_price: #{current_price}) / (intrinsic_value: #{intrinsic_value})"
       puts "intrinsic_value: (Minimum_Acceptable_Rate_Of_Return: #{Minimum_Acceptable_Rate_Of_Return}) ** (-Years_To_Grow: #{Years_To_Grow}) * (future_value: #{future_value})"
       puts "future_value: (future_eps: #{future_eps}) * (future_pe: #{future_pe})"
-      puts "future_eps: (1 + (eps_growth_rate: #{eps_growth_rate})) ** (Years_To_Grow: #{Years_To_Grow}) * (current_eps: #{current_eps})"
+      puts "future_eps: (1 + (growth_rate: #{growth_rate})) ** (Years_To_Grow: #{Years_To_Grow}) * (current_eps: #{current_eps})"
     end
 
     def to_hash
       (Attributes + [
-        :eps_growth_rate, :equity_growth_rate, :revenue_growth_rate, :free_cash_flow_growth_rate, :ten_year_growth_rate,
+        :eps_growth_rate, :growth_rate, :equity_growth_rate, :revenue_growth_rate, :free_cash_flow_growth_rate, :ten_year_growth_rate,
         :future_eps, :future_pe, :future_value, :intrinsic_value, :margin_of_safety
       ]).inject({}) { |hash, key| hash[key] = send(key); hash }
     end
